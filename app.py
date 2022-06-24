34b16e07633960dfb1e01659ed556aa60f1c750a
otabek2974 <otabekmamajonov2974@gmail.com>
2022-06-24 13:47:21 +0500

    oxirgi

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+import platform
+
+plt = platform.system()
+if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
+
+# title
+st.title('fish, bear, birdsni klassifikatsiya qiluvchi model')
+
+
+# rasmni joylash
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg'])
+if file:
+    st.image(file)
+    # PIL convert
+    img = PILImage.create(file)
+    # model
+    model = load_learner('model.pkl')
+
+    # prediction
+    pred, pred_id, probs=model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
+
+    # plotting
+    fig=px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
